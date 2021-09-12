3cb363c626b74b5dc1b8e7afe65f264dcb35c11a
TimSorgen <timothy.sorgen@bluewin.ch>
2021-09-12 20:29:51 +0200

    updated url

--- my.js
+++ my.js
@@ -14,7 +14,8 @@ function testGS(){
 function addGS(){
     //const url = "https://script.googleusercontent.com/macros/echo?user_content_key=ysfTQxTEVsOjGlE5HomgvoeaKcV3-Q05U4RM55HrDNkHtzFFianxAo3nrSXYCB1K720RTTLDUR0Ef6dvT9L_YP5Z4UY5h81mm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJWR1A_qaup48coehJkUXEO6rr4CCurBVLePZuhYRQqSSleP_x65anGoeqeeo0HVK_lHkGNNQ6mm7fM0GxMc5THg7-VTxnQ9f9z9Jw9Md8uu&lib=M2G_NM1iE-V7R8Qev1U-1xQwqzCun1r2R";
     //const url = "https://docs.google.com/spreadsheets/d/1veX9Y5YR47W0eD33mu1DOYBi_clpGnkhe78St9Vamdw/edit?usp=sharing";
-    const url = "https://script.google.com/macros/s/AKfycbwtFx0tj_sf9WEJYJXFvejmbbZXHwRWChyNAFPA-Q1QWp-ap0bq0v1IoB1dy9o-JEPzGA/exec";
+    //const url = "https://script.google.com/macros/s/AKfycbwtFx0tj_sf9WEJYJXFvejmbbZXHwRWChyNAFPA-Q1QWp-ap0bq0v1IoB1dy9o-JEPzGA/exec";
+    const url = "https://script.google.com/macros/s/AKfycbyWeUYwky-nQfHJBD-7dVs3k5SB-aqYiu5B6Z568aa3gE3ldFZT/exec";
     fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'no-cors', // no-cors, *cors, same-origin
