4ae327be638ab5056181ecdc64c895a143d97870
TimSorgen <timothy.sorgen@bluewin.ch>
2021-09-12 18:02:57 +0200

    Add files via upload

--- my.js
+++ my.js
@@ -19,14 +19,14 @@ function addGS(){
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'no-cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
+        //credentials: 'include', // include, *same-origin, omit
         headers: {
             'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         redirect: 'follow', // manual, *follow, error
         //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({name:"Sandro"}) // body data type must match "Content-Type" header
+        body: JSON.stringify({name:"Mike"}) // body data type must match "Content-Type" header
     });
 
 }
