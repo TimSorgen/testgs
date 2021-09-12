c367f46436c6693ccc771ac64b32882e49f88fd0
TimSorgen <timothy.sorgen@bluewin.ch>
2021-09-12 20:25:40 +0200

    including credentials

--- my.js
+++ my.js
@@ -19,7 +19,7 @@ function addGS(){
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'no-cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        //credentials: 'include', // include, *same-origin, omit
+        credentials: 'include', // include, *same-origin, omit
         headers: {
             'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
